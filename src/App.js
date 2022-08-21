20fcc2a96a4343eca5c9a490b9356c858d20a289
ALİ KELEŞ <alikeles60tokat@gmail.com>
2022-08-21 17:33:12 +0300

    Spring boot react bootcamp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {ToastContainer} from "react-toastify"
+//import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Routes, Route } from "react-router-dom";
+import BooksCard from "./components/BooksCard";
+import BookDetails from "./components/BookDetails";
+import AuthorDetails from "./components/AuthorDetails";
+import Authors from "./components/Authors";
+import Rentals from "./components/Rentals";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component() {
+  state ={};
+  render(){
+    return (
+      <React.Fragment>
+        <ToastContainer/>
+        <Navbar/>
+        <main className="container">
+   { 
+   <Routes>
+       <Route path="/" component={App} />
+    <Route path="/books/:id" component={BookDetails} />
+            <Route path="/authors/:id" component={AuthorDetails} />
+            <Route
+              path="/books"
+              render={props => <BooksCard {...props} />}
+            />
+            <Route path="/authors" render={props => 
+                <Authors {...props} />}
+            />
+            <Route path="/rentals" component={Rentals} />
+
+      </Routes> 
+      }
+
+      </main>
+      </React.Fragment>
+    );
+  }
+  
 }
 
 export default App;
